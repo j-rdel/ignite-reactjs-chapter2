82a4057121888fb5da44e7da55858a3e6415ae72
jardel <jardel.urban@ibridge.com.br>
2021-09-30 15:23:24 -0300

    Criando componente Dashboard, Header, Summary, TransactionTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import styled from 'styled-components';
+import { Dashboard } from './components/Dashboard';
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
 
 export function App() {
   return (
-    <div>
-      <Title>Hello World</Title>
+    <>
+      <Header />
+      <Dashboard/>
       <GlobalStyle />
-    </div>
+    </>
   );
 }
---
+++ src/components/TransactionTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./style";
+
+export function TransactionTable(){
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>01/10/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$1.100</td>
+                        <td>Casa</td>
+                        <td>15/09/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
